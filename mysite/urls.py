51cad95ef5f1aa3331af805f58b5ac414898eaa8
jyotman94 <jyotman94@gmail.com>
2016-03-03 16:34:41 +0530

    gif added

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^markdown/', include("django_markdown.urls")),
     url(r'', include('blog.urls')),
-    url(r'', include('parking.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
